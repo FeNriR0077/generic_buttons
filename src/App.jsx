9527ec07e14deb0535275ccd87fb686aaf99a4fc
FeNriR0077 <shardul.gautam123@gmail.com>
2023-03-24 16:57:45 +0545

    feat: added icon buttons

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
             FILLED
           </Button>
 
-          <Button  size="lg" color="secondary" variant={"outlined"} onClick={handleClick}>
+          <Button  size="lg" variant={"outlined"} onClick={handleClick}>
             OUTLINED
           </Button>
 
@@ -36,50 +36,154 @@ function App() {
             TEXT
           </Button>
 
-          <Button variant="text" size="md" onClick={handleClick} >
+          <Button variant="filled" size="md" onClick={handleClick} startIcon={<FontAwesomeIcon icon={faUser} />} >
+            USER
+          </Button>
+
+          <Button variant="text" size="md" onClick={handleClick} endIcon={<FontAwesomeIcon icon={faUser} />}>
+            USER
+          </Button>
+
+          <Button variant="outlined" size="md" onClick={handleClick} startIcon={<FontAwesomeIcon icon={faUser} />} endIcon={<FontAwesomeIcon icon={faUser} />}>
+            USER
+          </Button>
+
+          <Button variant="text" size="md" onClick={handleClick}>
             <FontAwesomeIcon icon={faUser} />
           </Button>
           
         </div>
 
-        {/* <div className="secondary-button-container button-container">
+        <div className="secondary-button-container button-container">
           <p>Secondary Color</p>
-          <Button variant={"filled"} color="secondary" onClick={handleClick} />
-          <Button
-            variant={"outlined"}
-            color="secondary"
-            onClick={handleClick}
-          />
-          <Button variant={"text"} color="secondary" onClick={handleClick} />
+          <Button variant={"filled"} color="secondary" onClick={handleClick} >
+            FILLED
+          </Button>
+
+          <Button  size="lg" color="secondary" variant={"outlined"} onClick={handleClick}>
+            OUTLINED
+          </Button>
+
+          <Button variant={"text"} color="secondary" onClick={handleClick} >
+            TEXT
+          </Button>
+
+          <Button variant="filled" color="secondary" size="md" onClick={handleClick} startIcon={<FontAwesomeIcon icon={faUser} />} >
+            USER
+          </Button>
+
+          <Button variant="text" color="secondary" size="md" onClick={handleClick} endIcon={<FontAwesomeIcon icon={faUser} />}>
+            USER
+          </Button>
+
+          <Button variant="outlined" color="secondary" size="md" onClick={handleClick} startIcon={<FontAwesomeIcon icon={faUser} />} endIcon={<FontAwesomeIcon icon={faUser} />}>
+            USER
+          </Button>
+
+          <Button variant="text" color="secondary" size="md" onClick={handleClick}>
+            <FontAwesomeIcon icon={faUser} />
+          </Button>
         </div>
 
         <div className="tertiary-button-container button-container">
           <p>Tertiary Color</p>
-          <Button variant={"filled"} color="tertiary" onClick={handleClick} />
-          <Button variant={"outlined"} color="tertiary" onClick={handleClick} />
-          <Button variant={"text"} color="tertiary" onClick={handleClick} />
+          <Button variant={"filled"} color="tertiary" onClick={handleClick} >
+            FILLED
+          </Button>
+
+          <Button  size="lg" variant={"outlined"} color="tertiary" onClick={handleClick}>
+            OUTLINED
+          </Button>
+
+          <Button variant={"text"} color="tertiary" onClick={handleClick} >
+            TEXT
+          </Button>
+
+          <Button variant="filled" color="tertiary" size="md" onClick={handleClick} startIcon={<FontAwesomeIcon icon={faUser} />} >
+            USER
+          </Button>
+
+          <Button variant="text" color="tertiary" size="md" onClick={handleClick} endIcon={<FontAwesomeIcon icon={faUser} />}>
+            USER
+          </Button>
+
+          <Button variant="outlined" color="tertiary" size="md" onClick={handleClick} startIcon={<FontAwesomeIcon icon={faUser} />} endIcon={<FontAwesomeIcon icon={faUser} />}>
+            USER
+          </Button>
+
+          <Button variant="text" color="tertiary" size="md" onClick={handleClick}>
+            <FontAwesomeIcon icon={faUser} />
+          </Button>
         </div>
 
         <div className="success-button-container button-container">
           <p>Success Color</p>
-          <Button variant={"filled"} color="success" onClick={handleClick} />
-          <Button variant={"outlined"} color="success" onClick={handleClick} />
-          <Button variant={"text"} color="success" onClick={handleClick} />
+                    <Button variant={"filled"} color="success" onClick={handleClick} >
+            FILLED
+          </Button>
+
+          <Button  size="lg" variant={"outlined"} color="success" onClick={handleClick}>
+            OUTLINED
+          </Button>
+
+          <Button variant={"text"} color="success" onClick={handleClick} >
+            TEXT
+          </Button>
+
+          <Button variant="filled" color="success" size="md" onClick={handleClick} startIcon={<FontAwesomeIcon icon={faUser} />} >
+            USER
+          </Button>
+
+          <Button variant="text" color="success" size="md" onClick={handleClick} endIcon={<FontAwesomeIcon icon={faUser} />}>
+            USER
+          </Button>
+
+          <Button variant="outlined" color="success" size="md" onClick={handleClick} startIcon={<FontAwesomeIcon icon={faUser} />} endIcon={<FontAwesomeIcon icon={faUser} />}>
+            USER
+          </Button>
+
+          <Button variant="text" color="success" size="md" onClick={handleClick}>
+            <FontAwesomeIcon icon={faUser} />
+          </Button>
         </div>
 
         <div className="error-button-container button-container">
           <p>Error Color</p>
-          <Button variant={"filled"} color="error" onClick={handleClick} />
-          <Button variant={"outlined"} color="error" onClick={handleClick} />
-          <Button variant={"text"} color="error" onClick={handleClick} />
+                    <Button variant={"filled"} color="error" onClick={handleClick} >
+            FILLED
+          </Button>
+
+          <Button  size="lg" variant={"outlined"} color="error" onClick={handleClick}>
+            OUTLINED
+          </Button>
+
+          <Button variant={"text"} color="error" onClick={handleClick} >
+            TEXT
+          </Button>
+
+          <Button variant="filled" color="error" size="md" onClick={handleClick} startIcon={<FontAwesomeIcon icon={faUser} />} >
+            USER
+          </Button>
+
+          <Button variant="text" color="error" size="md" onClick={handleClick} endIcon={<FontAwesomeIcon icon={faUser} />}>
+            USER
+          </Button>
+
+          <Button variant="outlined" color="error" size="md" onClick={handleClick} startIcon={<FontAwesomeIcon icon={faUser} />} endIcon={<FontAwesomeIcon icon={faUser} />}>
+            USER
+          </Button>
+
+          <Button variant="text" color="error" size="md" onClick={handleClick}>
+            <FontAwesomeIcon icon={faUser} />
+          </Button>
         </div>
 
         <div className="button-container">
           From Children
-          <Button buttonText="hi" variant={"filled"} color="error" onClick={handleClick} >
+          <Button variant={"filled"} color="error" onClick={handleClick} >
             Children
           </Button> 
-        </div> */}
+        </div> 
       </div>
     </div>
   );
